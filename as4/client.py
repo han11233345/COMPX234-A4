b33a5c38fcadb9b9db309763280e38125c48f566
HAN <ohyeahwww@163.com>
2025-06-14 19:13:21 +0800

    Update client.py
    Modify the client code to send download requests and receive responses

--- as4/client.py
+++ as4/client.py
@@ -3,9 +3,11 @@ import socket
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("127.0.0.1", 9999))
-    client.send("Hello, Server!".encode())
-    response = client.recv(4096)
-    print(response.decode())
+    filename = "example.txt"
+    client.send(f"DOWNLOAD {filename}".encode())
+
+    response = client.recv(4096).decode()
+    print(response)
 
     if __name__ == "__main__":
         main()
